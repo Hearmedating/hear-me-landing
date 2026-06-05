47b8fda366fdc8ac46655d53086858cf8e535d22
Hearmedating <hello@hearmedating.com>
2026-06-05 16:28:50 +0200

    Add page view tracking

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
-import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { Stack, usePathname } from 'expo-router';
 import { I18nProvider } from '../src/lib/i18n';
+import { trackPageView } from '../src/lib/analytics';
+
+function AnalyticsTracker() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    trackPageView(pathname || '/');
+  }, [pathname]);
+
+  return null;
+}
 
 export default function RootLayout() {
   return (
     <I18nProvider>
+      <AnalyticsTracker />
       <Stack screenOptions={{ headerShown: false }} />
     </I18nProvider>
   );
