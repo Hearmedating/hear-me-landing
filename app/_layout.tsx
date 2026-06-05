5445b99091a1a0c9501ee15e1993465c42f1362d
Hearmedating <hello@hearmedating.com>
2026-06-06 00:26:38 +0200

    Add Meta Pixel to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,19 @@ function AnalyticsTracker() {
 
     (window as any).gtag('js', new Date());
     (window as any).gtag('config', 'G-316LCDZKZQ');
+       if (!(window as any).fbq) {
+  const metaScript = document.createElement('script');
+  metaScript.async = true;
+  metaScript.src = 'https://connect.facebook.net/en_US/fbevents.js';
+  document.head.appendChild(metaScript);
+
+  (window as any).fbq = function () {
+    ((window as any).fbq.q = (window as any).fbq.q || []).push(arguments);
+  };
+
+  (window as any).fbq('init', '1747046096657921');
+  (window as any).fbq('track', 'PageView');
+}
   }
 
   trackPageView(pathname || '/');
